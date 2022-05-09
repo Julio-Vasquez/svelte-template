9fe9fb34e76fc8730a40643a1c10869080a14cb8
Julio-Vasquez <jualvalitube@gmail.com>
2022-05-09 00:17:35 -0500

    feat(config): general update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
-  props: {
-    appName: import.meta.env.VITE_APP_NAME,
-  },
+  props: { appName: import.meta.env.VITE_APP_NAME },
 })
 
 export default app
